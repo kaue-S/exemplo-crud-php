2558d550b8de8c149d6e1999003d3fc7225f5608
Kauê Evangelista <kauesilva38@hotmail.com>
2023-08-29 08:31:38 -0300

    Update funcoes-fabricantes.php

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -2,9 +2,10 @@
 require_once "conecta.php";
 
 // Usada em fabricantes/visualizar.php
-function lerFabricantes( PDO $conexao ){
+function lerFabricantes(PDO $conexao): array
+{
     $sql = "SELECT * FROM fabricantes ORDER BY nome";
-    
+
     try {
         /* Método prepare(): 
         Faz uma preparação/pré-config do comando SQL e
@@ -20,22 +21,23 @@ function lerFabricantes( PDO $conexao ){
         associativo. */
         $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
     } catch (Exception $erro) {
-        die("Erro: ".$erro->getMessage());
-    }    
+        die("Erro: " . $erro->getMessage());
+    }
 
     return $resultado;
 } // fim lerFabricantes
 
 
 // Usada em fabricantes/inserir.php
-function inserirFabricante(PDO $conexao, string $nomeDoFabricante){
+function inserirFabricante(PDO $conexao, string $nomeDoFabricante): void
+{
     /* :qualquerCoisa -> isso indica um "named parameter" 
     (parâmetro nomeado) */
     $sql = "INSERT INTO fabricantes(nome) VALUES(:nome)";
 
     try {
         $consulta = $conexao->prepare($sql);
-        
+
         /* bindValue() -> permite vincular o valor existente no
         parâmetro nomeado (:nome) à consulta que será executada.
         É necessário indicar qual é o parâmetro (:nome), de onde
@@ -45,14 +47,14 @@ function inserirFabricante(PDO $conexao, string $nomeDoFabricante){
 
         $consulta->execute();
     } catch (Exception $erro) {
-        die("Erro ao inserir: ".$erro->getMessage());
+        die("Erro ao inserir: " . $erro->getMessage());
     }
-
 } // fim inserirFabricante
 
 
 // Usada em fabricantes/atualizar.php
-function lerUmFabricante(PDO $conexao, int $idFabricante){
+function lerUmFabricante(PDO $conexao, int $idFabricante): array
+{
     $sql = "SELECT * FROM fabricantes WHERE id = :id";
 
     try {
@@ -61,35 +63,36 @@ function lerUmFabricante(PDO $conexao, int $idFabricante){
         $consulta->execute();
         $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
     } catch (Exception $erro) {
-        die("Erro ao carregar: ".$erro->getMessage());
+        die("Erro ao carregar: " . $erro->getMessage());
     }
 
     return $resultado;
 } // fim lerUmFabricante
 
 /* Exercício: IMPLEMENTE A FUNÇÃO ABAIXO */
-function atualizarFabricante(PDO $conexao, string $nome, int $id){
+function atualizarFabricante(PDO $conexao, string $nome, int $id): void
+{
     $sql = "UPDATE fabricantes SET nome = :nome WHERE id = :id";
-    
+
     try {
         $consulta = $conexao->prepare($sql);
         $consulta->bindValue(":nome", $nome, PDO::PARAM_STR);
         $consulta->bindValue(":id", $id, PDO::PARAM_INT);
         $consulta->execute();
     } catch (Exception $erro) {
-        die("Erro ao atualizar: ".$erro->getMessage());
+        die("Erro ao atualizar: " . $erro->getMessage());
     }
 } // fim atualizarFabricantes
 
 // Usada em fabricantes/excluir.php
-function excluirFabricantes(PDO $conexao, int $id) {
+function excluirFabricantes(PDO $conexao, int $id): void
+{
     $sql = "DELETE FROM fabricantes WHERE id = :id";
     try {
         $consulta = $conexao->prepare($sql);
         $consulta->bindValue(":id", $id, PDO::PARAM_INT);
         $consulta->execute();
     } catch (Exception $erro) {
-        die("Erro ao excluir: ".$erro->getMessage());
+        die("Erro ao excluir: " . $erro->getMessage());
     }
 } // fim excluirFabricantes
-
